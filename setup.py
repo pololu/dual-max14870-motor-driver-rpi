0f349638164fdbf06ea3dd2262363d99d1cdbcd6
Kevin (Pololu) <dev-kevin@pololu.com>
2018-04-04 01:58:38 +0000

    update version in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='dual_max14870_rpi',
-      version='1.0.0',
+      version='1.0.1',
       description=('Library for the Pololu Dual MAX14870 Motor '
                    'Driver Kit for Raspberry Pi'),
       url='http://www.pololu.com/product/3758',
